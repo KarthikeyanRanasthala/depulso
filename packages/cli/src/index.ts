eaa7bc7f9728146f63dfd4a0ad5ef400c0de0461
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2022-12-13 21:36:33 +0530

    Setup commander for parsing args

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
-const text: string = "From Depulso CLI";
+import { Command } from "commander";
+import { version } from "../package.json";
 
-console.log(text);
+const program = new Command();
+
+program.version(version);
+
+program
+  .command("login")
+  .description("Logs into your account")
+  .action(() => console.log("Login"));
+
+program
+  .command("init")
+  .description("Inits a new project")
+  .action(() => console.log("Init"));
+
+program
+  .command("deploy")
+  .description("Deploys the current project")
+  .action(() => console.log("Deploy"));
+
+program
+  .command("logout")
+  .description("Logs out of your account")
+  .action(() => console.log("Logout"));
+
+program.parse(process.argv);
