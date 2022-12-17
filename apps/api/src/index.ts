5754bd7c70ccaa4b4610b770d385738607545498
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2022-12-17 13:58:13 +0530

    Add config API with zod validation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  SUPABASE_ANON_KEY: z.string().min(1),
+  SUPABASE_URL: z.string().url(),
+  AUTH_ORIGIN: z.string().url(),
+  MAX_DEPLOYMENT_SIZE: z.string().min(1),
+});
+
+const env = envSchema.parse(process.env);
 
 const app = express();
 
 app.get("/", (_: Request, res: Response) => {
   res.json({ message: "Hello, API!" });
+});
+
+app.get("/config", (_: Request, res: Response) => {
+  res.json({
+    supabaseAnonKey: env.SUPABASE_ANON_KEY,
+    supabaseURL: env.SUPABASE_URL,
+    authOrigin: env.AUTH_ORIGIN,
+    maxDeploymentSize: env.MAX_DEPLOYMENT_SIZE,
+  });
 });
 
 const port = 1234;
