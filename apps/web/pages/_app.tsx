eb99c4c8e4eba843ae913120705c6a2ad4bfb7e0
mansisindhu <mansisindhu2909@gmail.com>
2022-12-19 10:14:59 +0530

    Add og meta data and favicon

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -21,7 +21,51 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Depulso</title>
+        <title>Depulso | Deploy your static site in seconds</title>
+        <meta
+          name="description"
+          content="Easy, one command deployments with free depulso.site subdomains."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://depulso.co/" />
+        <meta
+          property="og:title"
+          content="Depulso | Deploy your static site in seconds"
+        />
+        <meta
+          property="og:description"
+          content="Easy, one command deployments with free depulso.site subdomains."
+        />
+        <meta property="og:image" content="/og-image.png" />
+
+        <meta
+          property="twitter:title"
+          content="Depulso | Deploy your static site in seconds"
+        />
+        <meta
+          property="twitter:description"
+          content="Easy, one command deployments with free depulso.site subdomains."
+        />
+        <meta property="twitter:image" content="/og-image.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <SessionContextProvider supabaseClient={supabase}>
